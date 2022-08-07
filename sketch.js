9c04ebee72f38368713fe52247907df371374d1a
Zarra1 <110766368+Zarra1@users.noreply.github.com>
2022-08-07 20:25:00 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var box 
+function setup() {
+  createCanvas(400,400);
+  box = createSprite (200,200,50,50)
+  box.velocityX = 5
+
+}
+
+function draw() 
+{ 
+  background("green");
+drawSprites()
+}
+
+
+
+
